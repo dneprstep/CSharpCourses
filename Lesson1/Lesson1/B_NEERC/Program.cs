28a9a6ab28e8cffc38aa40bbd1af929116a9f751
step <stepets@ua.fm>
2015-06-10 23:05:19 +0300

    Add B and C

---
+++ Lesson1/Lesson1/B_NEERC/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B_NEERC
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint institutes,halls,result=0;
+
+            Console.Write("Enter number of institutes");
+            institutes = Convert.ToUInt16(Console.ReadLine());
+            if (institutes > 10000)
+                institutes = 10000;
+
+            Console.Write("Enter number of halls");
+            halls = Convert.ToUInt16(Console.ReadLine());
+            if (halls > 10000)
+                halls = 10000;
+
+            uint[] commands = new uint[institutes];
+            Random rand=new Random();
+            for(int i=0;i<institutes;i++)
+            {
+                commands[i]=Convert.ToUInt16( rand.Next( Convert.ToUInt16(halls+halls)));
+            }
+            Console.WriteLine("Number of commands from institutes");
+            foreach(uint i in commands)
+            {
+                Console.WriteLine(i);
+            }
+
+            for (int i = 0; i < commands.Length; i++)
+			{
+                if(commands[i]>halls)
+                    result+=halls;
+                else
+                    result+=commands[i];
+			}
+            Console.WriteLine("Max number of commands{0}",result);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lesson1/Lesson1/C_MinMaxSumm/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_MinMaxSumm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int elements,max=0,min=0;
+            Console.Write("Enter number of elements:");
+            elements = Convert.ToInt16(Console.ReadLine());
+            if (elements < 2)
+                elements = 2;
+            if (elements > 200000)
+                elements = 200000;
+
+            int[] mas = new int[elements];
+            Random rand = new Random();
+            for (int i = 0; i < elements; i++)
+            {
+                mas[i] = rand.Next(-10000, 10000);
+                Console.WriteLine(mas[i]);
+            }
+
+            for(int i=0;i<elements;i++)
+            {
+                if (i % 2 != 0 && mas[i] < mas[min])
+                    min = i;
+                if (i % 2 == 0 && mas[i] > mas[max])
+                    max = i;
+            }
+
+            Console.WriteLine("Min:{0}, Max{1}", min, max);
+            Console.WriteLine("MinElement:{0}, MaxElement:{1}", mas[min], mas[max]);
+
+            Console.WriteLine("Summa:{0}", mas[min] + mas[max]);
+            Console.ReadKey();
+        }
+    }
+}
