20a8bee34680706c2c4a2bc238d4c7d018a94623
step <stepets@ua.fm>
2015-06-19 14:58:38 +0300

    Add Lesson2 (modificators)

---
+++ Lesson1/Lesson1/G_NumberExpansion/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G_NumberExpansion
+{
+    class Program
+    {            
+        static int count = 0;
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number:");
+            int num = Convert.ToInt16(Console.ReadLine());
+
+            int i = 1, max = 0, temp = 0, summ = 0; 
+
+            while ((i/2)<=(num/2))
+            {
+                if(((i%2)==1) && ((num%i)==1))
+                {
+                    max = i;
+                    temp = (num / i) - (i / 2);
+                }
+                if((i%2)==0 && ((num%i)*2)==i)
+                {
+                    max = i;
+                    temp = (num / i) - (i / 2) + 1;
+                }
+                i++;
+            }
+
+            for (int j = temp; j < temp - 1 + max; )
+                summ += j;
+
+            Console.WriteLine(summ);
+ 
+  //          Console.WriteLine(rect(num,1));
+            Console.ReadKey();
+        }
+        static double rect(double num,int i)
+        {                    
+
+            if (num < 2)
+                return 1;
+
+            int first = (int) num / 2;
+            int second = (int) num - first;
+
+            Console.WriteLine("{0} {1} i:{2}", first, second, i);
+
+            rect(first,i++);
+            rect(second,i++);
+
+            return i;
+        }
+    }
+}
---
+++ Lesson2/Interval/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interval
+{
+    public struct int_interval
+    {
+        private int left;
+        private int right;
+        private bool start_include;
+        private bool end_include;
+        private List<char> hooks;
+
+        private void init_borders()
+        {
+            hooks = new List<char>();
+            hooks.Add('(');
+            hooks.Add(')');
+            hooks.Add('[');
+            hooks.Add(']');
+        }
+/*        public int_interval()
+        {
+            left = right = 0;
+            start_include = false;
+            end_include = false;
+            hooks = new List<char>();
+            hooks.Add('(');
+            hooks.Add(')');
+            hooks.Add('[');
+            hooks.Add(']');
+        }
+  */      public int_interval(int l, int r)
+        {
+            left = l;
+            right = r;
+            start_include = false;
+            end_include = false;
+            hooks = new List<char>();
+            hooks.Add('(');
+            hooks.Add(')');
+            hooks.Add('[');
+            hooks.Add(']');
+        }
+        public void set_interval(int l, int r)
+        {
+            left = l;
+            right = r;
+        }
+        public void set_include(bool start, bool end)
+        {
+            start_include = start;
+            end_include = end;
+        }
+        public bool is_in_interval(int number)
+        {
+            bool lResult, rResult;
+            if (start_include)
+                lResult = (number >= left) ? true : false;
+            else
+                lResult = (number > left) ? true : false;
+
+            if (end_include)
+                rResult = (number <= right) ? true : false;
+            else
+                rResult = (number < right) ? true : false;
+
+            return (lResult && rResult);
+        }
+        public static bool tryParse(string inString, out int_interval result)
+        {
+            bool start, end;
+            start = end = false;
+            inString.Trim();
+            string[] symbols = new string[2];
+
+            symbols = inString.Split(',');
+
+
+            //first element
+            if (symbols[0].First() == '(')
+                start = false;
+            else if (symbols[0].First() == '[')
+                start = true;
+            else
+            {
+                result = default(int_interval);
+                return false;
+            }
+            ///////////////////////
+
+            //last element
+            if (symbols[1].Last() == ')')
+                end = false;
+            else if (symbols[1].Last() == ']')
+                end = true;
+            else
+            {
+                result = default(int_interval);
+                return false;
+            }
+            ///////////////////////
+
+
+
+
+            result = default(int_interval);
+            return true;
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int_interval interval;
+            int_interval.tryParse("[23,45)",out interval);
+        }
+    }
+}
